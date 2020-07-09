e8092d2fbaca46a73529594d7d79633c86442f62
Denis Efremov <dagamitheslayer@gmail.com>
2020-07-09 19:52:49 +0300

    Updates

---
+++ precache-manifest.e247677b788a3b6d560916a709fc8ae6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce84ec27a3dbfc0660657efb62e7e15",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "6ea72bc11532f7a19d20",
+    "url": "/blog/static/css/main.85e318af.chunk.css"
+  },
+  {
+    "revision": "935c4f295ffb7414b8c5",
+    "url": "/blog/static/js/2.aa3837ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog/static/js/2.aa3837ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea72bc11532f7a19d20",
+    "url": "/blog/static/js/main.4d39a3ae.chunk.js"
+  },
+  {
+    "revision": "18587eccc92f26eed8a3",
+    "url": "/blog/static/js/runtime-main.a5ebe4ff.js"
+  },
+  {
+    "revision": "2f02a21a34054ddba3ac1abda0e0a41d",
+    "url": "/blog/static/media/back.2f02a21a.svg"
+  },
+  {
+    "revision": "89bbe1b21678099f1229ee4fd0aa6ecf",
+    "url": "/blog/static/media/header-bg.89bbe1b2.jpg"
+  }
+]);
